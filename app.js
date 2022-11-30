a1ceb35d9a1920800c164fc92d835224f796fad2
Riiyabatra <batra.riy@northeastern.edu>
2022-11-30 14:44:53 -0500

    Changes for assignment 9

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/user-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = 'mongodb+srv://riiyabatra:Riya@3579@cluster0.yv82osi.mongodb.net/?retryWrites=true&w=majority'
+    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
 app.use(cors())
@@ -10,4 +15,4 @@ app.use(express.json());
 TuitsController(app);
 HelloController(app)
 UserController(app)
-app.listen(process.env.PORT ||4000)
+app.listen(process.env.PORT || 4000)
